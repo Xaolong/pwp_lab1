79b110868630a5b530c01abaf7715bf7a30d61ec
Student <student@ib.polsl.pl>
2017-02-27 12:05:32 +0100

    Commit dzial 3.3 USE_TRIGONOMETRY_ON

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
+#include "LaborkaConfig.h" 
+#ifdef USE_TRIGONOMETRY_DEGREE
+#include "Trygonometria.h"
+#else
+#include <math.h>
+#endif
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <iostream>
+
 int main(){
+#ifdef USE_TRIGONOMETRY_DEGREE
+	double result = cos_degree(45.0);
+#else
+	double result = cos(M_PI/4.0);
+#endif
+	std::cout << result <<std::endl;
 return 0;
 }
